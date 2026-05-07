7484ef22e6442844b586b3c1c82a285e9576c83b
StefanMaker <coding_stefan@163.com>
2026-05-07 16:01:52 +0800

    fixed:merge wellknown_router

--- registry/src/registry/routers.py
+++ registry/src/registry/routers.py
@@ -14,7 +14,6 @@ from .api.v1.search_routes import router as search_router
 from .api.v1.server.server_routes import router as servers_router_v1
 from .api.v1.token_routes import router as token_router
 from .api.v1.workflow.workflow_routes import router as workflow_router
-from .api.wellknown_routes import router as wellknown_router
 from .core.config import settings
 from .health.routes import router as health_router
 
